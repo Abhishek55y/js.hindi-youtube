ea393a5ab847be91b82382bea99bcf1052101b6c
Abhishek55y <abhishek.25b01010299@abes.ac.in>
2026-08-04 09:10:50 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score="abhi"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+
+
+// "33"=>33
+// "33abc"=>NaN
+// true=>1
+// false=>0
+// null=>0
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// ""=>false
+// "abhi"=>true
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber);
+
+

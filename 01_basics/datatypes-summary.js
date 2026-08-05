b8ad827c7bee28c8929e32323a594fc098750ff6
Abhishek55y <abhishek.25b01010299@abes.ac.in>
2026-08-05 05:29:38 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+// Primitive
+
+// 7 types : String ,number ,Boolean,null,undefined,Symbol ,BigInt
+  
+const score=100
+const score=100.3
+
+const isLogged=false
+const outsideTemp=null
+let userEmail;
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId);
+
+
+
+
+// reference (non primitive)
+
+// Array, objects,funtions
+ const heros=["shaktiman","naagraj","doga"];
+ let myObj={
+    name ="abhi",
+   age=18,
+ }
+ const myFuntion=funtion()
+ {
+   console.log("hello world");
+   
+ }
+ console.log(typeof bigNumber);

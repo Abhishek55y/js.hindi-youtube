912a57f8977a294e6e6845b54e9e61ed6ac07dbc
Abhishek55y <abhishek.25b01010299@abes.ac.in>
2026-08-05 05:28:35 +0000

    conversionoperator

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,8 @@
-let score="abhi"
+// let score="abhi"
 
 // console.log(typeof score);
 // console.log(typeof(score));
-let valueInNumber=Number(score)
+// let valueInNumber=Number(score)
 // console.log(typeof valueInNumber);
 
 
@@ -11,13 +11,57 @@ let valueInNumber=Number(score)
 // true=>1
 // false=>0
 // null=>0
-let isLoggedIn=1
-let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn=1
+// let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 // ""=>false
-// "abhi"=>true
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(typeof stringNumber);
+// // "abhi"=>true
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(typeof stringNumber);
+
+// let value=3
+// let negValue=-value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+// let str1="hello"
+// let str2="abhi"
+// let str3=str1+str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+// console.log((3+4)*5%3);
+
+// console.log(+true); //allowed
+// console.log(true+); //not allowed
+
+let num1,num2,num3
+num1=num2=num3=2+2
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

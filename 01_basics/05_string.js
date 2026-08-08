c59f1095229db663c0639c9dbdcee36054c79b1c
Abhishek55y <abhishek.25b01010299@abes.ac.in>
2026-08-08 00:27:50 +0000

    String

---
+++ 01_basics/05_string.js
@@ -0,0 +1,41 @@
+// const name="abhis"
+// const repoCount=50
+
+// console.log(name+repoCount+"Value");
+
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameNmae=new String('abhishek')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameNamee.charAt(2))
+
+
+console.log(gameNmae.charAt(2));
+console.log(gameNmae.indexOf('t'));
+
+const newString = gameNmae.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameNmae.slice(-8, 4);
+console.log(anotherString);
+
+const newStringOne="  abhis  "
+console.log(newStringOne);
+console.log(newStringOne.trim());  //trim is remove strating space and end space
+
+const url="https://abhi.com/abhi%20yadav"
+
+console.log(url.replace('%20','-'))
+
+
+console.log(url.includes('abhis'));
+
+
+
+
+

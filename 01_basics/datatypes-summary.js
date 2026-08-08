33c8245586a8e6c4a6f37afec03d4fcbcb6967f3
Abhishek55y <abhishek.25b01010299@abes.ac.in>
2026-08-08 00:28:17 +0000

    datatypessummary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,15 +2,15 @@
 
 // 7 types : String ,number ,Boolean,null,undefined,Symbol ,BigInt
   
-const score=100
-const score=100.3
+// const score=100
+// const score=100.3
 
-const isLogged=false
-const outsideTemp=null
-let userEmail;
-const id=Symbol('123')
-const anotherId=Symbol('123')
-console.log(id===anotherId);
+// const isLogged=false
+// const outsideTemp=null
+// let userEmail;
+// const id=Symbol('123')
+// const anotherId=Symbol('123')
+// console.log(id===anotherId);
 
 
 
@@ -18,14 +18,43 @@ console.log(id===anotherId);
 // reference (non primitive)
 
 // Array, objects,funtions
- const heros=["shaktiman","naagraj","doga"];
- let myObj={
-    name ="abhi",
-   age=18,
- }
- const myFuntion=funtion()
- {
-   console.log("hello world");
+//  const heros=["shaktiman","naagraj","doga"];
+//  let myObj={
+//     name ="abhi",
+//    age=18,
+//  }
+//  const myFuntion=funtion()
+//  {
+//    console.log("hello world");
    
- }
- console.log(typeof bigNumber);
+//  }
+//  console.log(typeof bigNumber);
+
+
+
+
+//  +++++++++++++++++++++++++++++++++++++++++++
+
+// stack  (primitive), Heap  (Non-Primitive)
+
+let myYoutubename="abhiyadav.com"
+let anothername=myYoutubename
+anothername="chaicode"
+console.log(myYoutubename);
+
+console.log(anothername)
+
+let userOne={
+  email: "user@google.com",
+  upi: "user@ybl"
+}
+
+let usrTwo=userOne
+
+userTwo.email="abhi55@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+

8ece56efd93e8fe7ef2b989c8fb6cf45b63984c3
Abhishek55y <abhishek.25b01010299@abes.ac.in>
2026-08-09 10:05:03 +0000

    Json

---
+++ 02_basics/04_object.js
@@ -0,0 +1,65 @@
+// const tinderUser=new Object()
+const tinderUser={}
+
+tinderUser.id="123abc"
+tinderUser.name="Sammy"
+tinderUser.isLoggedIn=false
+
+// console.log(tinderUser);
+
+
+const regularUser={
+    email:"abhi@66gmail.com",
+    fullname: {
+        userfullname: {
+firstname:"abhi",
+lastname:"yadav"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1={1:"a",2:"b"}
+const obj2={3:'a',4:"b"}
+const obj4={5:'a',6:"b"}
+
+
+// const obj3={obj1,obj2}
+// const obj3=Object.assign({},obj1,obj2,obj4)
+const obj3={...obj1,...obj2}
+// console.log(obj3);
+
+const users=[
+    {
+        id:1,
+        email:"h@gmail.com"
+    }
+]
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"abhi"
+}
+// course.courseInstructor
+
+const {courseInstructor}=course
+console.log(courseInstructor);
+
+// {
+//     "name": "abhishek"
+//     "coursename":"js in hindi",
+//     "price":"free"
+// }
+
+[
+    {},
+    {},
+    {}
+]

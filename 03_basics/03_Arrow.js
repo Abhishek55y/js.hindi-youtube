b5be21e7411f538926ece742323d0625fbd8a7db
Abhishek55y <abhishek.25b01010299@abes.ac.in>
2026-08-13 02:14:50 +0000

    Arrow function

---
+++ 03_basics/03_Arrow.js
@@ -0,0 +1,52 @@
+const user ={
+    username:"abhishek",
+    price:999,
+    welcomeMessage: function(){
+        console.log(`${this.username},welcome to website`);
+        console.log(this);
+    }
+
+
+}
+// user.welcomeMessage()
+// user.username="sam"
+// user.welcomeMessage()
+        // console.log(this);
+
+        // function chai(){
+        //     let username="Abhishek"
+        //     console.log(this);
+            
+        // }
+        // chai()
+
+        // const chai()=function(){
+        //     let username="Abhishek"
+        //     console.log(this.username);
+            
+        // }
+        // chai()
+
+        const chai=()=>{
+            let username="Abhishek"
+            console.log(this);
+            
+        }
+
+
+        // chai()
+
+//    const addTwo= (num1,num2) => {
+//     return num1+num2
+//    }    
+
+//   const addTwo= (num1,num2) =>  num1+num2 //parenthisis
+    
+  const addTwo= (num1,num2) =>  ({username:"Abhishek"})
+   
+   console.log(addTwo(3,4));
+
+
+// const myArray=[2,5,3,7,8]
+
+// myArray.forEach()

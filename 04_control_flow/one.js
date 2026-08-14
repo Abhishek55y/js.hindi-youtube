2c6e96db68e21de9c38ef05c767f691cd47fddc9
Abhishek55y <abhishek.25b01010299@abes.ac.in>
2026-08-14 12:22:17 +0000

    if else condition

---
+++ 04_control_flow/one.js
@@ -0,0 +1,61 @@
+// //   if
+
+// const isUserloggedIn=true
+// const temperature=41
+//  if(temperature<50){
+//     console.log("less than 50");
+//  }
+//  else{
+//  console.log("temprature is greater than 50");
+//  }
+// if(2!=3){
+//     console.log("executed");
+    
+// }
+     
+
+// < ,> ,<=, >= ==, !=,===
+// const score=200
+// if(score>100){
+//     let power="fly" //agar hum let ki jagah var likh dete to code run ho jat but jydatar hame kevel "let and"const use karna hai
+
+//     console.log(`User power: ${power}`);
+    
+// }
+//     console.log(`User power: ${power}`);
+
+// const balance=1000
+   
+// if(balance>500) onsole.log("test");
+   
+// if(balance<500){
+// console.log("less than 500");
+
+// }
+// else if(balance<750)
+// {
+// console.log("less than 750");
+// }
+// else if(balance<900)
+// {
+//     console.log("less than 900");
+// }
+// else
+// {
+//    console.log("less than 1200"); 
+// }
+   
+
+
+const userLoggedIn=true
+const debitCard=true
+const loggedInFromGoogle=false
+const loggedInFromEmail=true
+
+if(userLoggedIn&&debitCard&& 2==3){
+    console.log("Allow to buy course");
+    
+}
+if(loggedInFromGoogle||loggedInFromEmail){
+    console.log("User logged in");
+}
